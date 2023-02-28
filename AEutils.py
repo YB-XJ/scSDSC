eef8e23ab9de4e3e0460c6dad2c41d5485bf319f
YB-free <108677002+YB-free@users.noreply.github.com>
2023-02-28 16:36:43 +0800

    Update AEutils.py

--- AEutils.py
+++ AEutils.py
@@ -60,24 +60,6 @@ def thrC(C,ro):
 
     return Cp
 
-# def sum_mareix(X):
-#     X=np.array(X)
-#     l=[]
-#     for j in range(X.shape[0]):
-#         sum1=0
-#         for i in range(X.shape[0]):
-#             if X[i][j]!=0:
-#                 sum1=sum1+1
-#         l.append(sum1)
-#     return np.diag(l)
-# def caculate_sum(eigenvalue):
-#     sum1=0
-#     l=np.array(eigenvalue)
-#     for i in range(len(l)):
-#         if l[i]==0:
-#             sum1 += 1
-#     return sum1
-
 def post_proC(C, K, d, alpha, ro):
     # C: coefficient matrix, K: number of clusters, d: dimension of each subspace
     #C = thrC(C,ro)
@@ -95,10 +77,6 @@ def post_proC(C, K, d, alpha, ro):
     L = np.abs(Z ** alpha)
     L = L/L.max()
     L = 0.5 * (L + L.T)
-    # D=sum_mareix(L)
-    # eigenvalue, featurevector = np.linalg.eig(L-D)
-    # num_zero=caculate_sum(eigenvalue)
-    # print('零特征值个数：',num_zero)
     spectral = cluster.SpectralClustering(n_clusters=K, eigen_solver='arpack', affinity='precomputed',assign_labels='discretize',random_state=22)
     spectral.fit(L)
     grp = spectral.fit_predict(L) + 1
@@ -151,7 +129,6 @@ def display1(Coef, subjs, d, alpha, ro,numc = None,label = None):
     return L,y_x
 
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k','orangered','greenyellow','darkolivegreen','maroon','darkgreen','darkslateblue','deeppink','goldenrod','teal','cornflowerblue','darksalmon','lightcoral','fuchsia']
-
 marks = ['o','+','.']
 def visualize(Img,Label,AE=None,filep=None):
     fig = plt.figure(figsize=(8, 8), dpi=300)
@@ -179,93 +156,6 @@ def visualize(Img,Label,AE=None,filep=None):
         plt.savefig(filep)
     plt.show()
 
-
-def visualize2(Img,Label):
-    fig = plt.figure(figsize=(8, 8), dpi=150)
-    ax1 = fig.add_subplot(111)
-    n = Img.shape[0]
-    lbs = np.unique(Label)
-    if Img.shape[1]>2:
-        Z_emb = TSNE(n_components=2).fit_transform(Img, Label)
-    else:
-        Z_emb = Img
-    for ii in range(lbs.size):
-        Z_embi = Z_emb[[i for i in range(n) if Label[i] == lbs[ii]]].transpose()
-        # print(Z_embi)
-        ax1.scatter(Z_embi[0], Z_embi[1], color=colors[ii % 10], marker=marks[ii // 10], label=str(ii),s=6)
-    ax1.legend()
-    plt.show()
-
-def visualize3(Img,Label,CAE=None,filep=None):
-    fig = plt.figure(figsize=(8, 8), dpi=150)
-    ax1 = fig.add_subplot(111)
-    n = Img.shape[0]
-    # if CAE is not None:
-    #     bs = CAE.batch_size
-    #     Z = CAE.transform(Img[:bs,:])
-    #     Z = np.zeros([Img.shape[0], Z.shape[1]])
-    #     for i in range(Z.shape[0] // bs):
-    #         Z[i * bs:(i + 1) * bs, :] = CAE.transform(Img[i * bs:(i + 1) * bs, :])
-    #     if Z.shape[0] % bs > 0:
-    #         Z[-bs:, :] = CAE.transform(Img[-bs:, :])
-    # else:
-    #     Z = Img
-    Z_emb = TSNE(n_components=2, metric='precomputed').fit_transform(Img, Label)
-    # print(Z_emb)
-    lbs = np.unique(Label)
-    for ii in range(lbs.size):
-        Z_embi = Z_emb[[i for i in range(n) if Label[i] == lbs[ii]]].transpose()
-        # print(Z_embi)
-        ax1.scatter(Z_embi[0], Z_embi[1], color=colors[ii % 10], marker=marks[ii // 10], label=str(ii),s=3)
-    ax1.legend()
-    if filep is not None:
-        plt.savefig(filep)
-    plt.show()
-
-def NNtest(Img,Label,teImg,teLabel,CAE=None,n_neigh=1,km=False):
-    Label = np.ravel(Label)
-    teLabel = np.ravel(teLabel)
-    n = Img.shape[0]
-    if CAE is not None:
-        Z = CAE.transform(Img)
-        teZ = np.zeros([teImg.shape[0],Z.shape[1]])
-        bs = CAE.batch_size
-        for i in range(teZ.shape[0]//bs):
-            teZ[i*bs:(i+1)*bs,:] = CAE.transform(teImg[i*bs:(i+1)*bs,:])
-        if teZ.shape[0]%bs>0:
-            teZ[-bs:,:] = CAE.transform(teImg[-bs:, :])
-    else:
-        print('raw')
-        Z = np.reshape(Img,[Img.shape[0],-1])
-        teZ = np.reshape(teImg,[teImg.shape[0],-1])
-    clf = neighbors.KNeighborsClassifier(n_neighbors=n_neigh)
-    clf.fit(Z, Label)
-    pre = clf.predict(teZ)
-    missrate_x, NMI, purity = err_rate(teLabel, pre)
-    acc_x = 1 - missrate_x
-    print("NN accuracy: %.4f" % acc_x)
-    print("NN NMI: %.4f" % NMI, "NN purity: %.4f" % purity)
-    if km:
-        acc_km = KMtest(teZ,teLabel,None)
-        return acc_x,acc_km
-    return acc_x
-
-def KMtest(Img,Label,CAE=None):
-    Label = np.ravel(Label)
-    n = Img.shape[0]
-    if CAE is not None:
-        Z = CAE.transform(Img)
-    else:
-        print('raw')
-        Z = np.reshape(Img,[Img.shape[0],-1])
-    clf = cluster.KMeans(n_clusters=np.unique(Label).shape[0])
-    lb = clf.fit_predict(Z)
-    missrate_x, NMI, purity = err_rate(Label, lb)
-    acc_x = 1 - missrate_x
-    print("KM accuracy: %.4f" % acc_x)
-    print("KM NMI: %.4f" % NMI, "KM purity: %.4f" % purity)
-    return acc_x
-
 def drawC(C,name='C-L2.png',norm=False):
     C = np.abs(C)
     C = C * (np.ones_like(C)-np.eye(C.shape[0]))
@@ -279,40 +169,7 @@ def drawC(C,name='C-L2.png',norm=False):
     plt.show()
     IC.save(name)
     # IC.show()
-
-
-def cosine(x1,x2):
-    dot1 = tf.reduce_sum(tf.multiply(x1,x2))
-    dot2 = tf.sqrt(tf.reduce_sum(tf.square(x1)))
-    dot3 = tf.sqrt(tf.reduce_sum(tf.square(x2)))
-    max_ = K.maximum(dot2*dot3, K.epsilon())
-    return dot1 / max_
-
-def cosinea(X,batch_size):
-    b=[]
-    #t=[]
-    for i in range (batch_size):
-        a = []
-        #n = []
-        for j in range (batch_size):
-            f=cosine(X[i],X[j])
-            #onel=np.ones(shape=f.shape)
-            #zerol=np.zeros(shape=f.shape)
-            a.append(f)
-            #n.append(tf.where(tf.reduce_max(f)>0.9,onel,zerol))
-        b.append(a)
-        #t.append(n)
-    c=tf.reshape(b,[batch_size,batch_size])
-    #d=tf.reshape(t,[batch_size,batch_size])
-
-    return c
-
-def euclidean_distance(x,y):
-    '''
-    Computes the euclidean distances between x and y
-    '''
-    return K.sqrt(K.maximum(K.sum(K.square(x - y), keepdims=True), K.epsilon()))
-
+    
 def contrastive_loss(y,d):
     tmp = y*tf.square(d)
     tmp2= (1-y)*tf.square(tf.maximum((1-d),0))
